a16bec4d2e1a9954e0bab79187060d1d085cc63c
derElektrobesen <pberejnoy2005@gmail.com>
2014-06-16 22:07:44 +0400

    Neuro network store implemented

--- src/facerecognizer.cpp
+++ src/facerecognizer.cpp
@@ -7,231 +7,159 @@
 #   define STORE_DEBUG_IMAGE(__img__, __name__)
 #endif
 
+enum fann_props_ids {
+    FANN_ACT_FUNCS_ID = 1,
+    FANN_ACT_STEEP_ID,
+    FANN_LAYERS_ID,
+    FANN_SCALE_MEAN_IN,
+    FANN_SCALE_DEVIATION_IN,
+    FANN_SCALE_NEW_MIN_IN,
+    FANN_SCALE_FACTOR_IN,
+    FANN_SCALE_MEAN_OUT,
+    FANN_SCALE_DEVIATION_OUT,
+    FANN_SCALE_NEW_MIN_OUT,
+    FANN_SCALE_FACTOR_OUT
+};
+
 void FaceRecognizer::load_fann_db() {
 
 }
 
 void FaceRecognizer::store_fann_db() {
     fann *ann = neuro_network;
-
+    QString query = "insert or replace into fann_params(name, value) values ";
 
     struct fann_layer *layer_it;
-    int calculated_decimal_point = 0;
     struct fann_neuron *neuron_it, *first_neuron;
     fann_type *weights;
     struct fann_neuron **connected_neurons;
     unsigned int i = 0;
 
-#ifndef FIXEDFANN
-    /* variabels for use when saving floats as fixed point variabels */
-    unsigned int decimal_point = 0;
-    unsigned int fixed_multiplier = 0;
-    fann_type max_possible_value = 0;
-    unsigned int bits_used_for_max = 0;
-    fann_type current_max_value = 0;
-#endif
+    DECLARE_SQL_CON(q);
 
-#ifndef FIXEDFANN
-    if(save_as_fixed)
-    {
-        /* save the version information */
-        fprintf(conf, FANN_FIX_VERSION "\n");
-    }
-    else
-    {
-        /* save the version information */
-        fprintf(conf, FANN_FLO_VERSION "\n");
-    }
-#else
-    /* save the version information */
-    fprintf(conf, FANN_FIX_VERSION "\n");
-#endif
+#define Q_NO_COMMA_VAR(__var__, __name__, __val__) \
+    ({ __var__ += QString() + "(" + __name__ + ", '" + QString::number(__val__) + "')"; })
 
-#ifndef FIXEDFANN
-    if(save_as_fixed)
-    {
-        /* calculate the maximal possible shift value */
+#define Q_S_VAR(__var__, __name__, __val__) \
+    ({ __var__ += QString() + ", ('" + __name__ + "', '" + __val__ + "')"; })
 
-        for(layer_it = ann->first_layer + 1; layer_it != ann->last_layer; layer_it++)
-        {
-            for(neuron_it = layer_it->first_neuron; neuron_it != layer_it->last_neuron; neuron_it++)
-            {
-                /* look at all connections to each neurons, and see how high a value we can get */
-                current_max_value = 0;
-                for(i = neuron_it->first_con; i != neuron_it->last_con; i++)
-                {
-                    current_max_value += fann_abs(ann->weights[i]);
-                }
-
-                if(current_max_value > max_possible_value)
-                {
-                    max_possible_value = current_max_value;
-                }
-            }
-        }
-
-        for(bits_used_for_max = 0; max_possible_value >= 1; bits_used_for_max++)
-        {
-            max_possible_value /= 2.0;
-        }
-
-        /* The maximum number of bits we shift the fix point, is the number
-         * of bits in a integer, minus one for the sign, one for the minus
-         * in stepwise, and minus the bits used for the maximum.
-         * This is devided by two, to allow multiplication of two fixed
-         * point numbers.
-         */
-        calculated_decimal_point = (sizeof(int) * 8 - 2 - bits_used_for_max) / 2;
-
-        if(calculated_decimal_point < 0)
-        {
-            decimal_point = 0;
-        }
-        else
-        {
-            decimal_point = calculated_decimal_point;
-        }
-
-        fixed_multiplier = 1 << decimal_point;
-
-#ifdef DEBUG
-        printf("calculated_decimal_point=%d, decimal_point=%u, bits_used_for_max=%u\n",
-               calculated_decimal_point, decimal_point, bits_used_for_max);
-#endif
-
-        /* save the decimal_point on a seperate line */
-        fprintf(conf, "decimal_point=%u\n", decimal_point);
-    }
-#else
-    /* save the decimal_point on a seperate line */
-    fprintf(conf, "decimal_point=%u\n", ann->decimal_point);
-
-#endif
+#define Q_VAR(__var__, __name__, __val__)   Q_S_VAR(__var__, __name__, QString::number(__val__))
+#define Q_S(__name__, __val__)              Q_S_VAR(query, __name__, __val__)
+#define Q(__name__, __val__)                Q_S(__name__, QString::number(__val__))
 
     /* Save network parameters */
-    fprintf(conf, "num_layers=%d\n", (int)(ann->last_layer - ann->first_layer));
-    fprintf(conf, "learning_rate=%f\n", ann->learning_rate);
-    fprintf(conf, "connection_rate=%f\n", ann->connection_rate);
-    fprintf(conf, "network_type=%u\n", ann->network_type);
+    Q_NO_COMMA_VAR(query, "'num_layers'", (int)(ann->last_layer - ann->first_layer));
+    Q("learning_rate", ann->learning_rate);
+    Q("connection_rate", ann->connection_rate);
+    Q("network_type", ann->network_type);
 
-    fprintf(conf, "learning_momentum=%f\n", ann->learning_momentum);
-    fprintf(conf, "training_algorithm=%u\n", ann->training_algorithm);
-    fprintf(conf, "train_error_function=%u\n", ann->train_error_function);
-    fprintf(conf, "train_stop_function=%u\n", ann->train_stop_function);
-    fprintf(conf, "cascade_output_change_fraction=%f\n", ann->cascade_output_change_fraction);
-    fprintf(conf, "quickprop_decay=%f\n", ann->quickprop_decay);
-    fprintf(conf, "quickprop_mu=%f\n", ann->quickprop_mu);
-    fprintf(conf, "rprop_increase_factor=%f\n", ann->rprop_increase_factor);
-    fprintf(conf, "rprop_decrease_factor=%f\n", ann->rprop_decrease_factor);
-    fprintf(conf, "rprop_delta_min=%f\n", ann->rprop_delta_min);
-    fprintf(conf, "rprop_delta_max=%f\n", ann->rprop_delta_max);
-    fprintf(conf, "rprop_delta_zero=%f\n", ann->rprop_delta_zero);
-    fprintf(conf, "cascade_output_stagnation_epochs=%u\n", ann->cascade_output_stagnation_epochs);
-    fprintf(conf, "cascade_candidate_change_fraction=%f\n", ann->cascade_candidate_change_fraction);
-    fprintf(conf, "cascade_candidate_stagnation_epochs=%u\n", ann->cascade_candidate_stagnation_epochs);
-    fprintf(conf, "cascade_max_out_epochs=%u\n", ann->cascade_max_out_epochs);
-    fprintf(conf, "cascade_min_out_epochs=%u\n", ann->cascade_min_out_epochs);
-    fprintf(conf, "cascade_max_cand_epochs=%u\n", ann->cascade_max_cand_epochs);
-    fprintf(conf, "cascade_min_cand_epochs=%u\n", ann->cascade_min_cand_epochs);
-    fprintf(conf, "cascade_num_candidate_groups=%u\n", ann->cascade_num_candidate_groups);
+    Q("learning_momentum", ann->learning_momentum);
+    Q("training_algorithm", ann->training_algorithm);
+    Q("train_error_function", ann->train_error_function);
+    Q("train_stop_function", ann->train_stop_function);
+    Q("cascade_output_change_fraction", ann->cascade_output_change_fraction);
+    Q("quickprop_decay", ann->quickprop_decay);
+    Q("quickprop_mu", ann->quickprop_mu);
+    Q("rprop_increase_factor", ann->rprop_increase_factor);
+    Q("rprop_decrease_factor", ann->rprop_decrease_factor);
+    Q("rprop_delta_min", ann->rprop_delta_min);
+    Q("rprop_delta_max", ann->rprop_delta_max);
+    Q("rprop_delta_zero", ann->rprop_delta_zero);
+    Q("cascade_output_stagnation_epochs", ann->cascade_output_stagnation_epochs);
+    Q("cascade_candidate_change_fraction", ann->cascade_candidate_change_fraction);
+    Q("cascade_candidate_stagnation_epochs", ann->cascade_candidate_stagnation_epochs);
+    Q("cascade_max_out_epochs", ann->cascade_max_out_epochs);
+    Q("cascade_min_out_epochs", ann->cascade_min_out_epochs);
+    Q("cascade_max_cand_epochs", ann->cascade_max_cand_epochs);
+    Q("cascade_min_cand_epochs", ann->cascade_min_cand_epochs);
+    Q("cascade_num_candidate_groups", ann->cascade_num_candidate_groups);
 
-#ifndef FIXEDFANN
-    if(save_as_fixed)
-    {
-        fprintf(conf, "bit_fail_limit=%u\n", (int) floor((ann->bit_fail_limit * fixed_multiplier) + 0.5));
-        fprintf(conf, "cascade_candidate_limit=%u\n", (int) floor((ann->cascade_candidate_limit * fixed_multiplier) + 0.5));
-        fprintf(conf, "cascade_weight_multiplier=%u\n", (int) floor((ann->cascade_weight_multiplier * fixed_multiplier) + 0.5));
+    Q("bit_fail_limit", ann->bit_fail_limit);
+    Q("cascade_candidate_limit", ann->cascade_candidate_limit);
+    Q("cascade_weight_multiplier", ann->cascade_weight_multiplier);
+
+    Q("cascade_activation_functions_count", ann->cascade_activation_functions_count);
+
+    q.exec("delete from fann_e_params");
+    QString tmp = "insert into fann_e_params(param_type, value) values ";
+    for(i = 0; i < ann->cascade_activation_functions_count; i++) {
+        if (i)
+            tmp += ", ";
+        Q_NO_COMMA_VAR(tmp, FANN_ACT_FUNCS_ID, ann->cascade_activation_functions[i]);
     }
-    else
-#endif
-    {
-        fprintf(conf, "bit_fail_limit="FANNPRINTF"\n", ann->bit_fail_limit);
-        fprintf(conf, "cascade_candidate_limit="FANNPRINTF"\n", ann->cascade_candidate_limit);
-        fprintf(conf, "cascade_weight_multiplier="FANNPRINTF"\n", ann->cascade_weight_multiplier);
+    if (i)
+        q.exec(tmp);
+
+    Q("cascade_activation_steepnesses_count", ann->cascade_activation_steepnesses_count);
+    tmp = "insert into fann_e_params(param_type, value) values ";
+    for(i = 0; i < ann->cascade_activation_steepnesses_count; i++) {
+        if (i)
+            tmp += ", ";
+        Q_NO_COMMA_VAR(tmp, FANN_ACT_STEEP_ID, ann->cascade_activation_steepnesses[i]);
     }
+    if (i)
+        q.exec(tmp);
 
-    fprintf(conf, "cascade_activation_functions_count=%u\n", ann->cascade_activation_functions_count);
-    fprintf(conf, "cascade_activation_functions=");
-    for(i = 0; i < ann->cascade_activation_functions_count; i++)
-        fprintf(conf, "%u ", ann->cascade_activation_functions[i]);
-    fprintf(conf, "\n");
-
-    fprintf(conf, "cascade_activation_steepnesses_count=%u\n", ann->cascade_activation_steepnesses_count);
-    fprintf(conf, "cascade_activation_steepnesses=");
-    for(i = 0; i < ann->cascade_activation_steepnesses_count; i++)
-    {
-#ifndef FIXEDFANN
-        if(save_as_fixed)
-            fprintf(conf, "%u ", (int) floor((ann->cascade_activation_steepnesses[i] * fixed_multiplier) + 0.5));
-        else
-#endif
-            fprintf(conf, FANNPRINTF" ", ann->cascade_activation_steepnesses[i]);
-    }
-    fprintf(conf, "\n");
-
-    fprintf(conf, "layer_sizes=");
-    for(layer_it = ann->first_layer; layer_it != ann->last_layer; layer_it++)
-    {
+    tmp = "insert into fann_e_params(param_type, value) values ";
+    i = 0;
+    for(layer_it = ann->first_layer; layer_it != ann->last_layer; layer_it++) {
+        if (i++)
+            tmp += ", ";
         /* the number of neurons in the layers (in the last layer, there is always one too many neurons, because of an unused bias) */
-        fprintf(conf, "%d ", (int)(layer_it->last_neuron - layer_it->first_neuron));
+        Q_NO_COMMA_VAR(tmp, FANN_LAYERS_ID, (int)(layer_it->last_neuron - layer_it->first_neuron));
     }
-    fprintf(conf, "\n");
+    if (i)
+        q.exec(tmp);
 
-#ifndef FIXEDFANN
-    /* 2.1 */
-    #define SCALE_SAVE( what, where )										\
-        fprintf( conf, #what "_" #where "=" );								\
-        for( i = 0; i < ann->num_##where##put; i++ )						\
-            fprintf( conf, "%f ", ann->what##_##where[ i ] );				\
-        fprintf( conf, "\n" );
+#define SCALE_SAVE( what, where, type) ({                                       \
+        QString tmp = "insert into fann_e_params(param_type, value) values ";   \
+        uint i;                                                                 \
+        for (i = 0; i < ann->num_##where##put; i++) {                           \
+            if (i)                                                              \
+                tmp += ", ";                                                    \
+            Q_NO_COMMA_VAR(tmp, type, ann->what##_##where[i]);                  \
+        }                                                                       \
+        if (i)                                                                  \
+            q.exec(tmp);                                                        \
+})
 
-    if(!save_as_fixed)
-    {
-        if(ann->scale_mean_in != NULL)
-        {
-            fprintf(conf, "scale_included=1\n");
-            SCALE_SAVE( scale_mean,			in )
-            SCALE_SAVE( scale_deviation,	in )
-            SCALE_SAVE( scale_new_min,		in )
-            SCALE_SAVE( scale_factor,		in )
+    if(ann->scale_mean_in != NULL) {
+        Q_S("scale_included", "1");
+        SCALE_SAVE( scale_mean, in, FANN_SCALE_MEAN_IN );
+        SCALE_SAVE( scale_deviation, in, FANN_SCALE_DEVIATION_IN );
+        SCALE_SAVE( scale_new_min, in, FANN_SCALE_NEW_MIN_IN );
+        SCALE_SAVE( scale_factor, in, FANN_SCALE_FACTOR_IN );
 
-            SCALE_SAVE( scale_mean,			out )
-            SCALE_SAVE( scale_deviation,	out )
-            SCALE_SAVE( scale_new_min,		out )
-            SCALE_SAVE( scale_factor,		out )
-        }
-        else
-            fprintf(conf, "scale_included=0\n");
-    }
+        SCALE_SAVE( scale_mean,	out, FANN_SCALE_MEAN_OUT );
+        SCALE_SAVE( scale_deviation, out, FANN_SCALE_DEVIATION_OUT );
+        SCALE_SAVE( scale_new_min, out, FANN_SCALE_NEW_MIN_OUT );
+        SCALE_SAVE( scale_factor, out, FANN_SCALE_FACTOR_OUT );
+    } else
+        Q_S("scale_included", "0");
+    q.exec("delete from fann_params");
+    q.exec(query);
+
 #undef SCALE_SAVE
-#endif
+#undef Q_NO_COMMA_VAR
+#undef Q_S_VAR
+#undef Q_VAR
+#undef Q_S
+#undef Q
 
-    /* 2.0 */
-    fprintf(conf, "neurons (num_inputs, activation_function, activation_steepness)=");
+    q.exec("delete from neurons");
+
+    i = 0;
+    query = "insert into neurons(num_inputs, activation_function, asctivation_steepness) values ";
     for(layer_it = ann->first_layer; layer_it != ann->last_layer; layer_it++)
-    {
-        /* the neurons */
-        for(neuron_it = layer_it->first_neuron; neuron_it != layer_it->last_neuron; neuron_it++)
-        {
-#ifndef FIXEDFANN
-            if(save_as_fixed)
-            {
-                fprintf(conf, "(%u, %u, %u) ", neuron_it->last_con - neuron_it->first_con,
-                        neuron_it->activation_function,
-                        (int) floor((neuron_it->activation_steepness * fixed_multiplier) + 0.5));
-            }
-            else
-            {
-                fprintf(conf, "(%u, %u, " FANNPRINTF ") ", neuron_it->last_con - neuron_it->first_con,
-                        neuron_it->activation_function, neuron_it->activation_steepness);
-            }
-#else
-            fprintf(conf, "(%u, %u, " FANNPRINTF ") ", neuron_it->last_con - neuron_it->first_con,
-                    neuron_it->activation_function, neuron_it->activation_steepness);
-#endif
+        for(neuron_it = layer_it->first_neuron; neuron_it != layer_it->last_neuron; neuron_it++) {
+            if (i++)
+                query += ", ";
+            query += "(" + QString::number(neuron_it->last_con - neuron_it->first_con) + ", " +
+                    QString::number(neuron_it->activation_function) + ", " +
+                    QString::number(neuron_it->activation_steepness) + ")";
         }
-    }
-    fprintf(conf, "\n");
+    if (i)
+        q.exec(query);
 
     connected_neurons = ann->connections;
     weights = ann->weights;
@@ -246,31 +174,18 @@ void FaceRecognizer::store_fann_db() {
      * Especially an iPAQ does not use the same binary
      * representation as an i386 machine.
      */
-    fprintf(conf, "connections (connected_to_neuron, weight)=");
-    for(i = 0; i < ann->total_connections; i++)
-    {
-#ifndef FIXEDFANN
-        if(save_as_fixed)
-        {
-            /* save the connection "(source weight) " */
-            fprintf(conf, "(%d, %d) ",
-                    (int)(connected_neurons[i] - first_neuron),
-                    (int) floor((weights[i] * fixed_multiplier) + 0.5));
-        }
-        else
-        {
-            /* save the connection "(source weight) " */
-            fprintf(conf, "(%d, " FANNPRINTF ") ", (int)(connected_neurons[i] - first_neuron), weights[i]);
-        }
-#else
-        /* save the connection "(source weight) " */
-        fprintf(conf, "(%d, " FANNPRINTF ") ", (int)(connected_neurons[i] - first_neuron), weights[i]);
-#endif
 
+    q.exec("delete from connections");
+
+    i = 0;
+    query = "insert into connections(connected_to_neuron, weight) values ";
+    for(i = 0; i < ann->total_connections; i++) {
+        if (i++)
+            query += ", ";
+        query += "(" + QString::number((int)(connected_neurons[i] - first_neuron)) + ", " + QString::number(weights[i]) + ")";
     }
-    fprintf(conf, "\n");
-
-    return calculated_decimal_point;
+    if (i)
+        q.exec(query);
 }
 
 FaceRecognizer::~FaceRecognizer() {
